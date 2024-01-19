d7cc779e0aa1c5a0e327d3bbd0dc1cb6d659c3f8
AlRezn <alrezn@gmail.com>
2024-01-20 00:21:12 +0300

    public

---
+++ src/main/java/com/stoloto/webstatic/StoLotoWebStaticApplication.java
@@ -0,0 +1,13 @@
+package com.stoloto.webstatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoLotoWebStaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoLotoWebStaticApplication.class, args);
+	}
+
+}
